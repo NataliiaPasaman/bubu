e9c7f5e4ff81b36a7f0bb58a634690fbb76e676b
NataliiaPasaman <natapasaman@gmail.com>
2023-06-05 22:48:51 +0300

    homepage

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { ReactComponent as Icon } from 'images/svg/user.svg';
+// import css from './HomePage.module.css';
+
+
+export default function Home() {
+    return (
+        <div>HOMEPAGE</div>
+    )
+}
